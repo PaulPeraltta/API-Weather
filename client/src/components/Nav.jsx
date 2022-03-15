c9e7ffa0e0b8c0b2c0dcb48903b6e748246c92d4
Your Name <you@example.com>
2022-03-14 22:11:03 -0300

    update title

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -7,17 +7,10 @@ export default function Nav({onSearch}) {
     <nav className={s.tutonav}>
       <div className={s.navposs}>
         <NavLink style={{textDecoration: "none"}} to="/">
-          {/* <div className={s.place}>
-            {/* <img
-              className={s.img}
-              src="https://img.icons8.com/color/48/000000/fog-day--v2.png"
-              alt="WeatherApp"
-            /> */}
             <div className={s.texti}>
               <h2 className={s.bold}>Weather</h2>
               <h2 className={s.fine}>App</h2>
             </div>
-          {/* </div> */}
         </NavLink>
         {/* <NavLink style={{textDecoration: "none"}} to="/about">
           <div className={s.textd}>
